ad2e65db153ad7bd2df649172db0803bca94cf94
Adrian Kalinin <adriankalinin@protonmail.com>
2023-01-09 23:36:55 +0200

    Set up user model on admin site

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from users import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin pages for user model"""
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "name", "password")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "name",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+    readonly_fields = ["last_login"]
+
+
+admin.site.register(models.User, UserAdmin)
