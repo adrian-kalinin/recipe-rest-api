9ed90c837841c5cdc9af2c1109ee2c000aa69a5e
Adrian Kalinin <adriankalinin@protonmail.com>
2023-01-15 18:54:09 +0200

    Implement Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """Database model for recipes"""
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    cooking_time = models.IntegerField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
